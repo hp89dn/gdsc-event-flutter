13a331faaa509b85a08ea1e0fdd07076c279a9f0
Dang Nhat Minh <dangnhatminh09032002@gmail.com>
2022-04-10 17:52:52 +0700

    Updates

---
+++ precache-manifest.1b67c695802e87ac86655c938e0e66ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62738c05457dc7ed71035a0127572971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411c1971fa599a562fde",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "411c1971fa599a562fde",
+    "url": "/static/js/2.79178dbd.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.79178dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48907fe2f68210cbf125",
+    "url": "/static/js/main.5124c01f.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
